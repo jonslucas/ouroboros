7d8a95c88ca0c7df4dd9f1995c067062bad90e22
Jon Lucas <jon.s.lucas@gmail.com>
2020-02-23 15:23:46 -0500

    feat: Added db auth and index routes.

--- src/app.js
+++ src/app.js
@@ -6,15 +6,18 @@ import mongoose from 'mongoose';
 
 mongoose.set('useCreateIndex', true);
 
-mongoose.connect('mongodb://localhost', {
+mongoose.connect('mongodb://localhost/ouroboros', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  user: 'snake',
+  pass: 'FiO6&2eM2@4t',
 });
 
 mongoose.connection.on('error', (err) => console.error(err));
 mongoose.connection.once('open', () => console.log('connected to database'));
 
 import usersRouter from './routes/users';
+import indexRouter from './routes/index';
 
 var app = express();
 
@@ -24,6 +27,7 @@ app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 export default app;
