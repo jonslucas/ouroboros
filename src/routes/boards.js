01e861a380a7746b3ab0797e65304be1bad1188f
Jon Lucas <jon.s.lucas@gmail.com>
2020-02-23 15:30:08 -0500

    feat: Added getBoard by Id middlewear.

--- src/routes/boards.js
+++ src/routes/boards.js
@@ -4,5 +4,19 @@ import Board from '../models/Boards';
 
 const router = Router();
 
+// Get Board from ID middlewear
+const getBoard = async (req, res, next) => {
+  try {
+    const board = await Board.findById(req.params.id);
+    if (!board) {
+      res.status(404).json({ message: 'Board Not Found' });
+    }
+    res.board = board;
+  } catch (e) {
+    res.status(500).json({ message: 'Invalid request' });
+  }
+
+  next();
+}
 
 export default router;
