626cab0acdb81ab3f5496eca13342d54fc8be454
Jon Lucas <jon.s.lucas@gmail.com>
2020-02-23 12:23:10 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+import express, { json, urlencoded, static } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import { connect, connection } from 'mongoose';
+
+connect('mongodb://localhost', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+connection.on('error', (err) => console.error(err));
+connection.once('open', () => console.log('connected to database'));
+
+import usersRouter from './routes/users';
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(static(join(__dirname, 'public')));
+
+app.use('/users', usersRouter);
+
+export default app;
