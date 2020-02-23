00d278b1113c6291152f338f3d0a92c4898ed0bc
Jon Lucas <jon.s.lucas@gmail.com>
2020-02-23 12:23:48 -0500

    feat: Added basic Board schema

---
+++ models/Boards.js
@@ -0,0 +1,20 @@
+import { Schema, model } from 'mongoose';
+
+const boardSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  columns: [new Schema({
+    title: String,
+    items: [new Schema({
+      label: String,
+      comments: [Schema.Types.ObjectId],
+      assignedTo: Schema.Types.ObjectId,
+      watchers: [Schema.Types.ObjectId],
+      tags: [Strings],
+    })]
+  })],
+});
+
+module.export = model('Board', boardSchema);
